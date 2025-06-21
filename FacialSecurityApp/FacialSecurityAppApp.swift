64d06b080e47b6c71eeed679ef09c8e41f76d878
bensonlag56 <bensonlag200@gmai.com>
2025-06-20 23:06:52 -0500

    Initial Commit

---
+++ FacialSecurityApp/FacialSecurityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialSecurityAppApp.swift
+//  FacialSecurityApp
+//
+//  Created by Benson Hilario on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FacialSecurityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FacialSecurityAppTests/FacialSecurityAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FacialSecurityAppTests.swift
+//  FacialSecurityAppTests
+//
+//  Created by Benson Hilario on 6/20/25.
+//
+
+import Testing
+@testable import FacialSecurityApp
+
+struct FacialSecurityAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
